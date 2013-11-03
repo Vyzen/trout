a202f3429d0585a503f863a94a391e5b3b2409ff
ichaelMay horsleyTay <ichaelmay.horsleytay@mailgay.omcay>
2013-11-03 06:01:47 -0500

    GPLv3ed everything.

--- Java/src/com/eigenvektor/matrix/DefaultMatrixElement.java
+++ Java/src/com/eigenvektor/matrix/DefaultMatrixElement.java
@@ -1,11 +1,27 @@
-package com.eigenvektor.matrix;
+/*
+ *  Default implementation of Matrix.Element
+ *  Copyright (C) 2013 Michael Thorsley
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
 
-import com.eigenvektor.matrix.Matrix.Element;
+package com.eigenvektor.matrix;
 
 /**
  * Simple implementation of the MatrixElement interface.
  */
-final class DefaultMatrixElement implements Element
+final class DefaultMatrixElement implements Matrix.Element
 {
 
 	private final int row;
@@ -35,6 +51,20 @@ final class DefaultMatrixElement implements Element
 	public double getValue()
 	{
 		return value;
+	}
+	
+	@Override
+	public boolean equals(final Object o)
+	{
+		if (!(o instanceof Matrix.Element)) { return false; }
+		Matrix.Element e = (Matrix.Element) o;
+		return e.getRow() == this.row && e.getCol() == this.col && e.getValue() == this.value;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return row ^ col ^ Double.valueOf(value).hashCode();
 	}
 	
 	@Override
