4f9499e7b31ad2184750bb6eccc5296e1060cd80
ichaelMay horsleyTay <ichaelmay.horsleytay@mailgay.omcay>
2013-11-07 06:43:24 -0500

    MutableMatrixOps override methods to ensure return of a Mutable matrix.
    Added GPL notice to scala stuff.

--- Scala/src/com/eigenvektor/matrix/MutableMatrixOps.scala
+++ Scala/src/com/eigenvektor/matrix/MutableMatrixOps.scala
@@ -1,10 +1,34 @@
+/*
+ *  Scala ops for mutable matrix.
+ *  Copyright (C) 2013 Michael Thorsley
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
 package com.eigenvektor.matrix
 
 /**
  * A wrapper for a matrix that scala-izes the basic operations.
  */
 final class MutableMatrixOps(private val mat:MutableMatrix) extends MatrixOps(mat) {
-	 def +=(m:Matrix) = mat.inPlaceAdd(m)
-	 def -=(m:Matrix) = mat.inPlaceSubtract(m)
-	 def *=(d:Double) = mat.inPlaceMultiply(d)
+  /// Override to specialize return type.
+  override def +(m:Matrix) = mat.add(m).asInstanceOf[MutableMatrix]
+  override def -(m:Matrix) = mat.subtract(m).asInstanceOf[MutableMatrix]
+  override def *(m:Matrix) = mat.multiply(m).asInstanceOf[MutableMatrix]
+  
+  // Additional in-place operations
+  def +=(m:Matrix) = mat.inPlaceAdd(m)
+  def -=(m:Matrix) = mat.inPlaceSubtract(m)
+  def *=(d:Double) = mat.inPlaceMultiply(d)
 }
--- Scala/src/com/eigenvektor/matrix/package.scala
+++ Scala/src/com/eigenvektor/matrix/package.scala
@@ -1,10 +1,28 @@
+/*
+ *  Scala ops for implicit conversions for matrices.
+ *  Copyright (C) 2013 Michael Thorsley
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
 package com.eigenvektor
 
 package object matrix {
-  
-	// Implicit conversion of Matrix to MatrixOps
-	implicit def matrixToMatrixOps(m:Matrix) = new MatrixOps(m)
 	
 	// Implict conversion of MutableMatrix to MutableMatrixOps
 	implicit def mutableMatrixToMutableMatrixOps(m:MutableMatrix) = new MutableMatrixOps(m)
+  
+	// Implicit conversion of Matrix to MatrixOps
+	implicit def matrixToMatrixOps(m:Matrix) = new MatrixOps(m)
 }
