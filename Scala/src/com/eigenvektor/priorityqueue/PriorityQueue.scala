72a9ee2acfdaa0a46bc8178e6d525797c35b9e79
ichaelMay horsleyTay <ichaelmay.horsleytay@mailgay.omcay>
2013-11-17 05:42:11 -0500

    Added PriorityQueue and test.

---
+++ Scala/src/com/eigenvektor/priorityqueue/PriorityQueue.scala
@@ -0,0 +1,67 @@
+/*
+ *  An implementation of a priority queue based on the binomial heap.
+ *  Copyright (C) 2013 Michael Thorsley
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
+package com.eigenvektor.priorityqueue
+
+/** A fully-persistent min-priority queue implemented using a binomial heap.
+ *  
+ *  @type E The type of element in the queue
+ *  @type P The type of prioirity in the queue
+ */
+final class PriorityQueue[E, P] private (private val heap:BinomialHeap[Pair[E, P]]) {
+
+  /** Creates a new, empty, priority queue */
+  def this(order:Ordering[P]) = this(new BinomialHeap[Pair[E,P]](Ordering.by((x:Pair[E,P]) => x._2)(order)))
+  
+  /** Inserts an element into this
+   *
+   *  @param element The element to add.
+   *  @param priority The priority of the new element.
+   *  @return The queue with <code>element</code> inserted at priority <code>priority</code>
+   */
+  def insert(element:E, priority:P) = new PriorityQueue[E,P](this.heap + (element, priority))
+  
+  /** Inserts an element-priority pair */
+  def +(element:Pair[E, P]) = insert(element._1, element._2)
+  
+  /** Gets the next element of the queue without removing it. */
+  def nextElement = heap.min._1
+  
+  /** Gets the next priority of the queue */
+  def nextPriority = heap.min._2
+  
+  /** Convenience method that gets the queue without returning the element as well */
+  def nextQueue = new PriorityQueue(heap.removeMin._2)
+  
+  /** Removes and returns the next element of the queue
+   *  
+   *  @return a triple consisting of the next element, its priority, and the queue after
+   *  that element is removed.
+   */
+  def pop = {
+    val ((element, priority), newHeap) = heap.removeMin
+    (element, priority, new PriorityQueue(newHeap))
+  }
+  
+  /** The size of the queue */
+  def size = heap.size
+  
+  /** Whether the queue is empty */
+  def isEmpty = heap.isEmpty
+  
+}
---
+++ Scala/test/com/eigenvektor/priorityqueue/TestPriorityQueue.scala
@@ -0,0 +1,76 @@
+/*
+ *  Copyright (C) 2013 Michael Thorsley
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
+package com.eigenvektor.priorityqueue
+
+import org.scalatest.FlatSpec
+import scala.collection.mutable.ListBuffer
+
+final class TestPriorityQueue extends FlatSpec {
+
+  "PriorityQueue" should "initailize to empty" in {
+    val pq = new PriorityQueue[Any, Int](Ordering.Int)
+    assert(pq.size == 0)
+    assert(pq.isEmpty)
+  }
+  
+  it should "accept a bunch of elements without failing" in {
+    var pq = new PriorityQueue[Any, Int](Ordering.Int)
+    for (j <- 0 to 1000) { pq = pq + ("Trout" + j, j) }
+    assert (pq.size == 1001)
+    assert (pq.nextElement == "Trout0")
+    assert (pq.nextPriority == 0)
+  }
+  
+  it should "remove a bunch of elements without failing" in {
+    var pq = new PriorityQueue[Int, Int](Ordering.Int)
+    for (j <- 0 to 1000) { pq = pq + (j + 1000,  j) }
+    
+    val elements = new ListBuffer[Int]
+    val priorities  = new ListBuffer[Int]
+    for (j <- 0 to 1000) {
+      elements += pq.nextElement
+      priorities += pq.nextPriority
+      pq = pq.nextQueue
+    }
+    
+    assert(pq.size == 0)
+    assert(pq.isEmpty)
+    assert (priorities.toList == (0 to 1000).toList)
+    assert (elements.toList == (1000 to 2000).toList)
+  }
+  
+  it should "order random data" in {
+    val r = new scala.util.Random(1337)
+    val l = List.fill(1000)(r.nextInt)
+    
+    var pq = new PriorityQueue[Int, Int](Ordering.Int)
+    for (x <- l) { pq = pq + (x,x) }
+    
+    val sorted = new ListBuffer[Int]
+    while(!pq.isEmpty) {
+      sorted += pq.nextElement
+      pq = pq.nextQueue
+    }
+    
+    for (pair <- sorted.toList.sliding(2))
+    {
+      assert(pair(1) >= pair(0))
+    }
+  }
+  
+}
