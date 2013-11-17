5ee30db6d6b14225c6e90e056a23af7d031cc608
ichaelMay horsleyTay <ichaelmay.horsleytay@mailgay.omcay>
2013-11-17 04:21:02 -0500

    Move the BinomialTree class into BinomialHeap.scala instead of having a separate file for it.

--- Scala/src/com/eigenvektor/priorityqueue/BinomialHeap.scala
+++ Scala/src/com/eigenvektor/priorityqueue/BinomialHeap.scala
@@ -21,6 +21,33 @@ package com.eigenvektor.priorityqueue
 
 import scala.collection.mutable.ListBuffer
 
+/** Implementation of a binomial tree with values
+ *  
+ *  Simple implementation with just an add method.
+ */
+final class BinomialTree[T](val value:T, val subtrees:List[BinomialTree[T]]) {
+  
+  /** Secondary constructor for a single-node binomial tree */
+  def this(value:T) = this(value, Nil)
+
+  /** The size of the binomial tree
+   *  
+   *  This is always 2^(number of subtrees)
+   */
+  def size = 1 << subtrees.size
+  
+  /** Merges another tree with this one.
+   *  
+   *  @param other the tree to merge.
+   *  @throws IllegalArgumentException if other is not the same size as this.
+   */
+  def merge(other:BinomialTree[T]) = {
+    require(other.size == this.size, "other must have the same size as this.")
+    new BinomialTree(value, other :: subtrees)
+    }
+  
+}
+
 /**
  * Implementation of a binomial heap.
  */
