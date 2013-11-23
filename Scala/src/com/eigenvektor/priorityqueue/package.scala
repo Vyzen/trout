f658320aec147cb7fac7e33292d67ec03a9e1fab
ichaelMay horsleyTay <ichaelmay.horsleytay@mailgay.omcay>
2013-11-23 17:38:16 -0500

    Doc update.

--- Scala/src/com/eigenvektor/priorityqueue/package.scala
+++ Scala/src/com/eigenvektor/priorityqueue/package.scala
@@ -2,6 +2,10 @@ package com.eigenvektor
 
 /** Package for a fully-persistent priority queue.
  * 
+ * The main class is [[PriorityQueue]], which is backed by a the skew-binomial heap
+ * implementation in [[SkewBinomialHeap]].  There is also a regular binomial heap
+ * implementation in [[BinomialHeap]].
+ * 
  */
 package object priorityqueue {
 
